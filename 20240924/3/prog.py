48842cc6e4209ccfd494487da8a9abe45dada2fb
vrodealyona <160181376+vrodealyona@users.noreply.github.com>
2024-09-30 23:16:56 +0300

    prog.py

--- 20240924/3/prog.py
+++ 20240924/3/prog.py
@@ -5,16 +5,17 @@ a.append(list(eval(input())))
 n = len(a[0])
 for i in range(1, n):
     a.append(list(eval(input())))
-print(a)
+
 for i in range(n):
     b.append(list(eval(input())))
-print(b)
-c = a
+
+d = []
 for i in range(n):
     for j in range(n):
-        c[i][j] = 0
+        s = 0
         for k in range(n):
-            c[i][j] = c[i][j] + a[i][k]*b[k][j]
-            print(a[i][k]*b[k][j], c[i][j], end = "|")
-        print()
-    print(*c[i], sep=",")
+            s = s + a[i][k]*b[k][j]
+        d.append(s)
+    c.append(d)
+    d = []
+    print(*c[i], sep = ',')
