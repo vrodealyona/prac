913738aaacbe4ad1890878ed2880af1758427077
vrodealyona <160181376+vrodealyona@users.noreply.github.com>
2024-10-03 11:22:29 +0300

    prog.py

---
+++ 20241001/1/prog.py
@@ -0,0 +1,11 @@
+def Pareto(*pairs):
+    res = []
+    for x in pairs:
+        for y in pairs:
+            if (x[0] <= y[0]) and (x[1]<=y[1]) and ((x[0] != y[0]) or (x[1] != y[1])):
+                break
+        else:
+            res.append(x)
+    return tuple(res)
+
+print(Pareto(eval(input())))
