3ed42372adf3a2f367893c1658829e3f6b25aa92
Алексеева Алёна <s02220014@vm-mz4-17.prac.cs.msu.ru>
2024-11-05 11:54:20 +0300

    Boo

---
+++ 20241105/2/prog.py
@@ -0,0 +1,9 @@
+class Triangle:
+	def __init__(self, a, b, c):
+		self.x1 = a
+		self.x2 = b
+		self.x3 = c
+	def __abs__(self):
+		if 2*max(self.x1, self.x2, self.x3) < sum(self.x1, self.x2, self.x3):
+			p = 
+	
